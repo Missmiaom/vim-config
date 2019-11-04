c0f4c9f6690a72c8a74071d79f299052920a76a8
leiyiming <leiyiming@cvte.com>
2019-11-04 20:24:05 +0800

    [init]

---
+++ .vimrc
@@ -0,0 +1,40 @@
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+set t_Co=256
+
+" syntastic start
+set statusline+=%#warningmsg#
+set statusline+=%{SyntasticStatuslineFlag()}
+set statusline+=%*
+
+let g:syntastic_always_populate_loc_list = 1
+let g:syntastic_auto_loc_list = 1
+let g:syntastic_check_on_open = 1
+let g:syntastic_check_on_wq = 0
+let g:ycm_show_diagnostics_ui = 0
+let g:syntastic_cpp_checkers = ['gcc']
+let g:syntastic_cpp_compiler = "gcc"
+let g:syntastic_cpp_compiler_options = "-std=c++11 -Wall -Wextra -Wpedantic"
+" syntastic end
+
+" nerdtree start
+" autocmd vimenter * NERDTree " 打开vim时，自动启动nerdtree
+autocmd StdinReadPre * let s:std_in=1
+autocmd VimEnter * if argc() == 0 && !exists("s:std_in") | NERDTree | endif 
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif " 关闭文件时自动关闭nerdtree
+" ctrl+n 打开或者关闭nerdtree
+map <C-n> :NERDTreeToggle<CR>
+" nerdtree end
+
+" ultisnips start
+" ultisnips end
+
+" YCM start
+let g:ycm_global_ycm_extra_conf = "~/.vim/.ycm_extra_conf.py"
+let g:ycm_key_list_select_completion=[]
+let g:ycm_key_list_previous_completion=[]
+" YCM end
+
+" airline start
+" airline end
---
+++ install.sh
@@ -0,0 +1,64 @@
+# update vim8
+yum install -y gcc gcc-c++ ruby ruby-devel lua lua-devel  \
+    ctags git python python-devel \
+    tcl-devel ncurses-devel \
+    perl perl-devel perl-ExtUtils-ParseXS \
+    perl-ExtUtils-CBuilder \
+    perl-ExtUtils-Embed
+    
+# update git
+yum remove git
+yum groupinstall "Development Tools"
+yum install gettext-devel openssl-devel perl-CPAN perl-devel zlib-devel
+yum install https://centos7.iuscommunity.org/ius-release.rpm
+yum install git2u-all
+
+wget https://github.com/vim/vim/archive/v8.1.2251.tar.gz
+cd vim-8.1.2251
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib64/python2.7/config \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-cscope \
+            --prefix=/usr/local
+make && make install
+ln -s /usr/bin/vim /usr/local/bin/vim
+
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# syntastic
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
+
+# nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+# ultisnips
+git clone git://github.com/SirVer/ultisnips.git ~/.vim/bundle/ultisnips
+git clone git://github.com/honza/vim-snippets.git ~/.vim/bundle/vim-snippets
+
+# YCM
+yum install python-devel
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer
+
+# airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+# autoclose
+git clone https://github.com/Townk/vim-autoclose.git  ~/.vim/bundle/vim-autoclose
+
+# fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+
+
+
+
+
+
